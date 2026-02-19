5c492760e37e11d313918f143b7b234ef05e10f7
Dmitriy <spaysonis@gmail.com>
2026-02-19 20:55:12 +0000

    add sessions

--- src/main/views_ajax.py
+++ src/main/views_ajax.py
@@ -8,11 +8,11 @@ def get_schedules_ajax(request, pk):
 
     selected_date = request.GET.get("date")
     selected_cinema = request.GET.get("cinema")
-    format_movie = request.GET.get("format")
+    selected_format = request.GET.get("format")
 
     print(selected_cinema)
     print(selected_date)
-    print(format_movie)
+    print(selected_format)
 
     schedules = Schedule.objects.filter(movie_id=pk)
 
@@ -20,17 +20,23 @@ def get_schedules_ajax(request, pk):
         parsed_date = datetime.strptime(selected_date, "%Y-%m-%d").date()
         schedules = schedules.filter(date=parsed_date)
 
+
     if selected_cinema:
         schedules = schedules.filter(hall__cinema_id=selected_cinema)
+
+
+    if selected_format:
+        schedules = schedules.filter(format__type=selected_format)
 
     schedules = schedules.select_related("hall", "format")
     data = [{
             "time": s.time.strftime("%H:%M"),
             "hall": s.hall.number,
             "format": s.format.type,
-            "price": s.price,
+            "price": s.price or 0,
     }
         for s in schedules
     ]
+
 
     return JsonResponse({"schedules": data})
