37181a4a4029bc6d9b95a0f9b7337b564e443476
Spayson <spayson@example.com>
2026-03-29 18:27:24 +0000

    bags webSocket

--- config/asgi.py
+++ config/asgi.py
@@ -1,30 +1,30 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+import django # Добавьте это
 from django.core.asgi import get_asgi_application
-
-
-from src.main.routing import websocket_urlpatterns as url_patterns_main
-from src.chat.routing import websocket_urlpatterns as url_patterns_chat
-from src.cms.routing import websocket_urlpatterns as url_pattern_mailing
-all_websocket_patterns =  url_patterns_chat + url_patterns_main + url_pattern_mailing
-
-
-django_asgi_app = get_asgi_application()
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Сначала инициализируем Django
+django_asgi_app = get_asgi_application()
+django.setup() # Добавьте это ПЕРЕД вашими кастомными импортами
+
+# Теперь импортируем всё остальное
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# Импорты ваших маршрутов
+from src.main.routing import websocket_urlpatterns as url_patterns_main
+from src.chat.routing import websocket_urlpatterns as url_patterns_chat
+from src.cms.routing import websocket_urlpatterns as url_pattern_mailing
+
+all_websocket_patterns = url_patterns_chat + url_patterns_main + url_pattern_mailing
+
+
+print("--- КАРТА ПУТЕЙ WEBSOCKET ---")
+for pattern in all_websocket_patterns:
+    print(f"Path: {pattern.pattern}")
+print("----------------------------")
 
 
 application = ProtocolTypeRouter(
